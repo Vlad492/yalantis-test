240e0951097037e2026f19b4ea0e60031a4a5599
Vlad492 <skrinka2604@gmail.com>
2020-07-22 16:19:57 +0300

    final commit

--- src/components/card.js
+++ src/components/card.js
@@ -1,24 +1,30 @@
 import React from 'react'
 
 const Card = (props) => {
-    let color = ''
-    if (props.count < 2) color = 'grey'
-    else if(props.count < 6) color = 'blue' 
-    else if(props.count < 10) color = 'green'
-    else color = 'red'
 
+    const setColor = (count) => {
+        if (count < 3) return 'grey'
+        else if (count < 7) return 'blue'
+        else if (count < 11) return 'green'
+        else return 'red'
 
+    }
 
+    let color = setColor(props.count)
     
+
+
+
+
     return (
-        <div className="month" style={{backgroundColor: color}}>
+        <div className="month" style={{ backgroundColor: color }}>
             <div className="container">
                 <h1>{props.month}</h1>
             </div>
-            <div className="popUp" style={{backgroundColor: color}}>
+            <div className="popUp" style={{ backgroundColor: color }}>
                 <div className="popUpContent">
                     <h2>{props.month}</h2>
-                    {props.users.map((elem, index) => <p key = {index}>{elem.firstName} {elem.lastName}</p>)}
+                    {props.users.map((elem, index) => <p key={index}>{elem.firstName} {elem.lastName}</p>)}
                 </div>
             </div>
         </div>
