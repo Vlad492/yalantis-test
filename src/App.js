2419c9ae2a35d64d32acde3df977bd9887f84254
Vlad492 <skrinka2604@gmail.com>
2020-08-03 13:39:51 +0300

    release

--- src/App.js
+++ src/App.js
@@ -2,40 +2,40 @@ import React from 'react';
 import './App.css';
 import Card from './components/card'
 import Preloader from './components/preloader'
-import{useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 
-function App(){
+function App() {
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-  const [users,setUsers] = useState()
-  const request = async()=>{
-    try{
+  const [users, setUsers] = useState()
+  const request = async () => {
+    try {
       let res = await fetch('https://yalantis-react-school-api.yalantis.com/api/task0/users').then((res) => res.json())
       setUsers(res)
-    }catch(e){
+    } catch (e) {
       console.log(e)
     }
   }
-useEffect(()=>{
-  request()
-},[])
-useEffect(()=>{
-  console.log(users)
-},[users])
+  useEffect(() => {
+    request()
+  }, [])
+  useEffect(() => {
+    console.log(users)
+  }, [users])
 
-if (users) {
-  return (
-    <div className="App">
-      <article>
-        <h1>Test app</h1>
-        {months.map((elem, index) => <Card month={elem} key={index} users={users.filter((user) => new Date(user.dob).getMonth() === index)} />)}
-      </article>
+  if (users) {
+    return (
+      <div className="App">
+        <article>
+          <h1>Test app</h1>
+          {months.map((elem, index) => <Card month={elem} key={index} users={users.filter((user) => new Date(user.dob).getMonth() === index)} />)}
+        </article>
 
-    </div>
-  )
-} else {
-  return (<Preloader />)
-}
+      </div>
+    )
+  } else {
+    return (<Preloader />)
+  }
 }
 
 
