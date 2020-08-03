9970fd5743792573253cf222a65041f5efb563f0
Vlad492 <skrinka2604@gmail.com>
2020-08-03 13:36:59 +0300

    release commit

--- src/App.js
+++ src/App.js
@@ -2,38 +2,42 @@ import React from 'react';
 import './App.css';
 import Card from './components/card'
 import Preloader from './components/preloader'
+import{useState,useEffect} from 'react'
 
 
-
-
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+function App(){
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const [users,setUsers] = useState()
+  const request = async()=>{
+    try{
+      let res = await fetch('https://yalantis-react-school-api.yalantis.com/api/task0/users').then((res) => res.json())
+      setUsers(res)
+    }catch(e){
+      console.log(e)
     }
   }
-  componentDidMount() {
-      fetch('https://yalantis-react-school-api.yalantis.com/api/task0/users')
-      .then((res) => res.json())
-      .then((res)=>this.setState({ users: res }))
-  }
-  render() {
-    if (this.state.users[0]) {
-      return (
-        <div className="App">
-          <article>
-            <h1>Test app</h1>
-            {this.state.months.map((elem, index) => <Card month={elem} key={index} users={this.state.users.filter((user) => new Date(user.dob).getMonth() === index)} />)}
-          </article>
+useEffect(()=>{
+  request()
+},[])
+useEffect(()=>{
+  console.log(users)
+},[users])
 
-        </div>
-      )
-    } else {
-      return (<Preloader />)
-    }
-  }
+if (users) {
+  return (
+    <div className="App">
+      <article>
+        <h1>Test app</h1>
+        {months.map((elem, index) => <Card month={elem} key={index} users={users.filter((user) => new Date(user.dob).getMonth() === index)} />)}
+      </article>
+
+    </div>
+  )
+} else {
+  return (<Preloader />)
 }
-export default App;
+}
+
+
+
+export default App; 
